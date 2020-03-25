726db69d6197507e59a6be2cb4cc8b59c4b2bfc5
Brian Ombega <brianombega@gmail.com>
2020-03-25 16:19:40 +0300

    imported Scanner object to allow user input in building stairs.

--- Practice/src/Staircase.java
+++ Practice/src/Staircase.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Staircase {
 
 	public static void main(String[] args) {
-		int numSteps = 5;
-		int stepWidth = 3;
+		//create an instant of the scanner object.
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the number of steps you desire: ");
+		int numSteps = scan.nextInt();
+		System.out.println("Please enter the width you want each step to have:");
+		int stepWidth = scan.nextInt();
 		for(int i = 0; i < numSteps ; i++ ) {
 			for(int j = 0; j < (i + 1) * stepWidth; j++ ) {
 				System.out.print("*");
