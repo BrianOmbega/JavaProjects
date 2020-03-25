cb2a99f6e68dab41f750f03bb599eb144a140a9b
Brian Ombega <brianombega@gmail.com>
2020-03-25 16:00:51 +0300

    imported java.text and casted earnings into DecimalFormat

--- InvestmentEarnings/src/investmentearnings/Driver.java
+++ InvestmentEarnings/src/investmentearnings/Driver.java
@@ -2,6 +2,7 @@ package investmentearnings;
 
 //import the scanner object to allow user input
 import java.util.Scanner;
+import java.text.*;
 
 public class Driver {
 	public static void main(String[] args) {
@@ -12,6 +13,9 @@ public class Driver {
 		double buyingPrice = scan.nextDouble();
 		//trigger the while loop by setting closingPrice as a positive number
 		double closingPrice = 0.1;
+		
+		DecimalFormat df = new DecimalFormat("0.00");
+		
 		//keep track of the number of days since the user entered the buying price
 		int day = 1;
 		
@@ -22,10 +26,10 @@ public class Driver {
 			double earnings = closingPrice - buyingPrice;
 			
 			if(earnings > 0) {
-				System.out.println("After day " + day + " you earned " + earnings + " per share from your original buying price.");				
+				System.out.println("After day " + day + " you earned " + df.format(earnings) + " per share from your original buying price.");				
 			}
 			else if(earnings < 0) {
-				System.out.println("After day "+ day + " you lost " + (-earnings) + " per share from your original buying price.");
+				System.out.println("After day "+ day + " you lost " + df.format(-earnings) + " per share from your original buying price.");
 			}
 			else {
 				System.out.println("After day " + day +" you have no earnings per share from your original buying price.");
